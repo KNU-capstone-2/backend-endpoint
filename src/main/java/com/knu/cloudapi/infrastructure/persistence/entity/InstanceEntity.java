c04c21855192d3a309a80a27bdfc274df24623b7
LEESEUNGYEOL <lsy12210@gmail.com>
2023-05-07 04:35:38 +0900

    feat: instance domain

---
+++ src/main/java/com/knu/cloudapi/infrastructure/persistence/entity/InstanceEntity.java
@@ -0,0 +1,21 @@
+package com.knu.cloudapi.infrastructure.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "instance")
+public class InstanceEntity {
+    @Id @GeneratedValue
+    @Column(name = "instance_id")
+    private Long id;
+    @Column(name = "instance")
+    private String name;
+    @Column(name = "access_role")
+    private String accessRole;
+}
