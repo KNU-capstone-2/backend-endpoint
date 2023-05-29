ff1742830fd28307304fe898ec709d6730734127
LEESEUNGYEOL <lsy12210@gmail.com>
2023-05-30 05:14:57 +0900

    fix: post keypair error

--- src/main/java/com/knu/cloudapi/application/dto/request/PostKeypairRequest.java
+++ src/main/java/com/knu/cloudapi/application/dto/request/PostKeypairRequest.java
@@ -2,9 +2,12 @@ package com.knu.cloudapi.application.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
-@Getter
+@NoArgsConstructor
+@Getter @Setter
 public class PostKeypairRequest {
     private String keypairName;
 
--- src/main/java/com/knu/cloudapi/application/dto/response/InstanceControlResponse.java
+++ src/main/java/com/knu/cloudapi/application/dto/response/InstanceControlResponse.java
@@ -2,9 +2,12 @@ package com.knu.cloudapi.application.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
-@Getter
+@NoArgsConstructor
+@Getter @Setter
 public class InstanceControlResponse {
     private boolean isSuccess;
 
