3f19aa506b70a52924ae1cb76e015a5c240f680c
ChangEn Yea <nsce9806q@gmail.com>
2023-05-30 03:44:51 +0900

    fix: wrap InstanceOptions API with ApiResponse

--- src/main/java/com/knu/cloudapi/adapter/in/web/InstanceOptionsController.java
+++ src/main/java/com/knu/cloudapi/adapter/in/web/InstanceOptionsController.java
@@ -1,7 +1,12 @@
 package com.knu.cloudapi.adapter.in.web;
 
 import com.knu.cloudapi.application.dto.request.PostKeypairRequest;
-import com.knu.cloudapi.application.dto.response.*;
+import com.knu.cloudapi.application.dto.response.ApiResponse;
+import com.knu.cloudapi.application.dto.response.GetFlavorResponse;
+import com.knu.cloudapi.application.dto.response.GetImageResponse;
+import com.knu.cloudapi.application.dto.response.GetKeypairResponse;
+import com.knu.cloudapi.application.dto.response.GetNetworkResponse;
+import com.knu.cloudapi.application.dto.response.PostKeypairResponse;
 import com.knu.cloudapi.application.port.in.GetProjectResponse;
 import com.knu.cloudapi.application.port.in.InstanceOptionsUsecase;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +17,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,49 +27,48 @@ public class InstanceOptionsController {
     private final InstanceOptionsUsecase instanceOptionsUsecase;
 
     @GetMapping("flavors")
-    public ResponseEntity<List<GetFlavorResponse>> getFlavorList() {
-        return ResponseEntity.ok().body(instanceOptionsUsecase.getFlavorList());
+    public ResponseEntity<ApiResponse<List<GetFlavorResponse>>> getFlavorList() {
+        return ResponseEntity.ok().body(new ApiResponse<>(1000, "OK", instanceOptionsUsecase.getFlavorList()));
     }
 
     @GetMapping("flavor/{id}")
-    public ResponseEntity<GetFlavorResponse> getFlavorList(@PathVariable("id") String id) {
-        return ResponseEntity.ok().body(instanceOptionsUsecase.getFlavorById(id));
+    public ResponseEntity<ApiResponse<GetFlavorResponse>> getFlavorList(@PathVariable("id") String id) {
+        return ResponseEntity.ok().body(new ApiResponse<>(1000, "OK", instanceOptionsUsecase.getFlavorById(id)));
     }
 
     @GetMapping("networks")
-    public ResponseEntity<List<GetNetworkResponse>> getNetworkList() {
-        return ResponseEntity.ok().body(instanceOptionsUsecase.getNetworkList());
+    public ResponseEntity<ApiResponse<List<GetNetworkResponse>>> getNetworkList() {
+        return ResponseEntity.ok().body(new ApiResponse<>(1000, "OK", instanceOptionsUsecase.getNetworkList()));
     }
 
     @GetMapping("network/{id}")
-    public ResponseEntity<GetNetworkResponse> getNetworkById(@PathVariable("id") String id) {
-        return ResponseEntity.ok().body(instanceOptionsUsecase.getNetworkById(id));
+    public ResponseEntity<ApiResponse<GetNetworkResponse>> getNetworkById(@PathVariable("id") String id) {
+        return ResponseEntity.ok().body(new ApiResponse<>(1000, "OK", instanceOptionsUsecase.getNetworkById(id)));
     }
 
     @GetMapping("keypairs")
-    public ResponseEntity<List<GetKeypairResponse>> getKeypairList() {
-        return ResponseEntity.ok().body(instanceOptionsUsecase.getKeypairList());
+    public ResponseEntity<ApiResponse<List<GetKeypairResponse>>> getKeypairList() {
+        return ResponseEntity.ok().body(new ApiResponse<>(1000, "OK", instanceOptionsUsecase.getKeypairList()));
     }
 
     @GetMapping("keypair/{id}")
-    public ResponseEntity<GetKeypairResponse> getKeypairById(@PathVariable("id") String id) {
-        return ResponseEntity.ok().body(instanceOptionsUsecase.getKeypairById(id));
+    public ResponseEntity<ApiResponse<GetKeypairResponse>> getKeypairById(@PathVariable("id") String id) {
+        return ResponseEntity.ok().body(new ApiResponse<>(1000, "OK", instanceOptionsUsecase.getKeypairById(id)));
     }
 
     @GetMapping("images")
-    public ResponseEntity<List<GetImageResponse>> getImageList() {
-        return ResponseEntity.ok().body(instanceOptionsUsecase.getImageList());
+    public ResponseEntity<ApiResponse<List<GetImageResponse>>> getImageList() {
+        return ResponseEntity.ok().body(new ApiResponse<>(1000, "OK", instanceOptionsUsecase.getImageList()));
     }
 
     @GetMapping("image/{id}")
-    public ResponseEntity<GetImageResponse> getImageById(@PathVariable("id") String id) {
-        return ResponseEntity.ok().body(instanceOptionsUsecase.getImageById(id));
+    public ResponseEntity<ApiResponse<GetImageResponse>> getImageById(@PathVariable("id") String id) {
+        return ResponseEntity.ok().body(new ApiResponse<>(1000, "OK", instanceOptionsUsecase.getImageById(id)));
     }
 
-
     @PostMapping("keypair")
-    public ResponseEntity<PostKeypairResponse> postKeypair(@RequestBody PostKeypairRequest request) {
-        return ResponseEntity.ok().body(instanceOptionsUsecase.postKeypair(request));
+    public ResponseEntity<ApiResponse<PostKeypairResponse>> postKeypair(@RequestBody PostKeypairRequest request) {
+        return ResponseEntity.ok().body(new ApiResponse<>(1000, "OK", instanceOptionsUsecase.postKeypair(request)));
     }
 
 
@@ -75,8 +78,8 @@ public class InstanceOptionsController {
     }
 
     @DeleteMapping("keypair/{id}")
-    public ResponseEntity<String> deleteKeypairById(@PathVariable("id") String id) {
-        return ResponseEntity.ok().body(instanceOptionsUsecase.deleteKeypair(id));
+    public ResponseEntity<ApiResponse<String>> deleteKeypairById(@PathVariable("id") String id) {
+        return ResponseEntity.ok().body(new ApiResponse<>(1000, "OK", instanceOptionsUsecase.deleteKeypair(id)));
     }
 
 }
